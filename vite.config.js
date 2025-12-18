3c89b7991fcb506128aaa5c7d738da9cde57f99c
Morgan Brown <git@email.mhgbrown.is>
2025-12-18 19:14:58 +0100

    Use empty string as base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '',
   server: {
     // Configure the dev server to fall back to index.html for client-side routing
     historyApiFallback: true
